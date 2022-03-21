bac3c4ab150deeffc530590fc73f19e8820c19c2
Zaccal <ksss90411@gmail.com>
2022-03-21 14:48:30 +0600

    make to error 404

---
+++ 404.php
@@ -0,0 +1,31 @@
+<?php
+header('HTTP/1.1 404 Not Found');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<style>
+		body{
+			background-color: #fff;
+			color: #333;
+			font-family: sans-serif;
+		}
+		.title{
+			font-size: 40px;
+			text-align: center;
+		}
+		.subtitle{
+			font-size: 20px;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<h1 class="title">404</h1>
+	<p class="subtitle">станица не найдена</p>
+</body>
+</html>
